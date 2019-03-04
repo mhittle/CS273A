916525ecab1d2c43e54150ced51160ea6a5399de
mhittle <mhittle@gmail.com>
2019-03-03 19:33:52 -0800

    minor edit

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import tabix
 #this will execute the mutationData.py file, which will build a BST from the clinvar.vcf file. Both files must be in the same directory. 
 #this will take 3-10 minutes to build. 
 
-import mutationData.py 
+import mutationData 
 
 #example: code below tests position 86952254 on chromosome 11 for a known diesease caused by mutation. Returns two objects, a boolean and a list of features related to the disease. 
 diseaseFound , diseaseInfo = testForDisease(11,86952254)
